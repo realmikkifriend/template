488f6758e1722aeca4963e8eddd70ccf7f116e70
Mikki Friend <realmikkifriend@gmail.com>
2025-12-22 00:26:47 -0600

    chore: corrected formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 <p>This app is ready to customize.</p>
 
 <p>
-	<strong
-		>{import.meta.env.VITE_EXAMPLE_VAR ||
-			'Environmental variables are not set up. Is there a `.env` file?'}</strong
-	>
+	<strong>
+		{import.meta.env.VITE_EXAMPLE_VAR ||
+			'Environmental variables are not set up. Is there a `.env` file?'}
+	</strong>
 </p>
