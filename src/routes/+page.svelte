05fd2568d0e3d180729e7924b4e8818d4dd05d44
Mikki Friend <realmikkifriend@gmail.com>
2026-01-06 21:32:41 -0600

    refactor: extracted variables section to new component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { exampleCount, incrementCount, resetCount } from '$lib/stores/example.svelte';
 	import WeatherSection from './weather/WeatherSection.svelte';
+	import VariablesSection from './variables/VariablesSection.svelte';
 </script>
 
 <section>
@@ -10,34 +10,9 @@
 </section>
 
 <section>
-	<h2>Persistent Stores</h2>
+	<h2>Variables</h2>
 
-	<div class="stat">
-		<div class="stat-title">Example Value</div>
-		<div class="stat-value">{exampleCount.current}</div>
-		<div class="stat-actions">
-			<button class="btn btn-sm btn-success" onclick={incrementCount} type="button"
-				>Increment</button
-			>
-			<button class="btn btn-dash btn-sm" onclick={resetCount} type="reset">Reset</button>
-		</div>
-	</div>
-</section>
-
-<section>
-	<h2>Environmental Variables</h2>
-
-	<p>
-		<strong>
-			{#if import.meta.env.VITE_EXAMPLE_VAR}
-				{import.meta.env.VITE_EXAMPLE_VAR}
-			{:else}
-				<span class="text-error-content">
-					Environmental variables are not set up. Is there a .env file?
-				</span>
-			{/if}
-		</strong>
-	</p>
+	<VariablesSection />
 </section>
 
 <section>
---
+++ src/routes/variables/VariablesSection.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { exampleCount, incrementCount, resetCount } from '$lib/stores/example.svelte';
+</script>
+
+<div class="flex flex-row gap-10">
+	<div>
+		<h3>Persistent Stores</h3>
+		<div class="stat">
+			<div class="stat-title">Example Value</div>
+			<div class="stat-value">{exampleCount.current}</div>
+			<div class="stat-actions">
+				<button class="btn btn-sm btn-success" onclick={incrementCount} type="button"
+					>Increment</button
+				>
+				<button class="btn btn-dash btn-sm" onclick={resetCount} type="reset">Reset</button>
+			</div>
+		</div>
+	</div>
+
+	<div>
+		<h3>Environmental Variables</h3>
+		<p>
+			<strong>
+				{#if import.meta.env.VITE_EXAMPLE_VAR}
+					{import.meta.env.VITE_EXAMPLE_VAR}
+				{:else}
+					<span class="text-error-content">
+						Environmental variables are not set up. Is there a .env file?
+					</span>
+				{/if}
+			</strong>
+		</p>
+	</div>
+</div>
